d4ba810aad8ba2005ad1eacbc4dbe8446cccd45e
Ryan Bales <ryanabales02@gmail.com>
2024-04-18 17:05:00 -0400

    Added LandlordDashboard functionality

--- WiseHousingHub/Data/IPropertyRepository.cs
+++ WiseHousingHub/Data/IPropertyRepository.cs
@@ -7,6 +7,7 @@ namespace WiseHousingHub.Data
         public void Add(Property property);
         public void Update(Property property);
         public Property GetById(int id);
+        public List<Property> GetPropertiesByLandlordId(int landlordId);
         public List<Property> GetAll();
         public void Delete(int id);
     }
