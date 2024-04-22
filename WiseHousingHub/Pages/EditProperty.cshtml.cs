204cd43ee6c0b0a896ed10d49ded0ce99353a8db
Ryan Bales <ryanabales02@gmail.com>
2024-04-22 19:14:39 -0400

    Added authorization to all required pages and changed "Price" on forms to "Price Per Month"

--- WiseHousingHub/Pages/EditProperty.cshtml.cs
+++ WiseHousingHub/Pages/EditProperty.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WiseHousingHub.Data;
@@ -5,6 +6,7 @@ using WiseHousingHub.Models;
 
 namespace WiseHousingHub.Pages
 {
+    [Authorize]
     public class EditPropertyModel : PageModel
     {
         private IPropertyRepository propertyRepo;
